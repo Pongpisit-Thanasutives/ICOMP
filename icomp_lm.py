21025cb5bad55eb90b118c96c29d6d1b5e4c1e66
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2024-03-17 20:55:58 +0900

    Update code

--- icomp_lm.py
+++ icomp_lm.py
@@ -3,7 +3,7 @@ import statsmodels.api as sm
 # Implemented by Pongpisit Thanasutives in 2024
 
 # beta = coefficients
-def llf_complexity(X_pre, y_pre, beta=None, a_n=None, include_bias=False):
+def llf_complexity(X_pre, y_pre, beta=None, a_n=None, include_bias=False, verbose=False):
     N = len(y_pre)
     if a_n is None:
         a_n = np.log(N)
@@ -24,12 +24,15 @@ def llf_complexity(X_pre, y_pre, beta=None, a_n=None, include_bias=False):
         S_inv = np.linalg.inv(X_pre.T@X_pre)
         llf = -0.5*(N*np.log(2*np.pi) + N*np.log(rss/N) + N)
 
-    C0 = np.trace(np.log(S_inv))-np.log(np.linalg.det(S_inv))
-    C1 = q*np.log(np.trace(S_inv)/q)-np.log(np.linalg.det(S_inv))
+    det = np.linalg.det(S_inv)
+    if verbose and det == 0.0: 
+        print("inf covariance complexity")
+    C0 = np.trace(np.log(S_inv))-np.log(det)
+    C1 = q*np.log(np.trace(S_inv)/q)-np.log(det)
     C_IFIM = (q+1)*np.log((np.trace(S_inv) + 2*rss/N)/(q+1)) - \
-                np.log(np.linalg.det(S_inv)) - np.log(2*rss/N)
+                np.log(det) - np.log(2*rss/N)
     C_COV = (q+1)*np.log((np.trace(S_inv) + 2*rss*(N-q)/(N**2))/(q+1)) - \
-                np.log(np.linalg.det(S_inv)) - np.log(2*rss*(N-q)/(N**2))
+                np.log(det) - np.log(2*rss*(N-q)/(N**2))
 
     C0 = C0/2
     C1 = C1/2
